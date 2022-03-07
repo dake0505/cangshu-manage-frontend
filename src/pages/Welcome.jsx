fcfee5cb5f8e8c0047b003ed28479837f7c7a295
dake0505 <dake0505@163.com>
2022-03-07 22:05:09 +0800

    init

---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PageContainer } from '@ant-design/pro-layout';
+
+const Welcome = () => {
+  return (
+    <PageContainer>
+      首页
+    </PageContainer>
+  );
+};
+
+export default Welcome;
